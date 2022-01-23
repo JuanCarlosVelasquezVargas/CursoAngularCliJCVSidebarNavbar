12b8330bb212c725939a80f915da0bcbdf7c60fb
[JuanCarlosVelásquezVargas] <[juanvelasquez7914@hotmail.com]>
2022-01-23 15:44:06 -0500

    Tarea Cronómetro

---
+++ src/app/acumulador/acumulador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acumulador',
+  templateUrl: './acumulador.component.html',
+  styleUrls: ['./acumulador.component.scss']
+})
+export class AcumuladorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
