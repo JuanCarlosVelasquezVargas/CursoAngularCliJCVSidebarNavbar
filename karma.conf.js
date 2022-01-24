612110803f5efccfc54e19a5de53c3b7035cd9ff
[JuanCarlosVelásquezVargas] <[juanvelasquez7914@hotmail.com]>
2022-01-24 00:06:54 -0500

    Tarea NabCar Sidebar GitHubPages

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/curso-angular-cli-jcv'),
+      dir: require('path').join(__dirname, './coverage/docs'),
       subdir: '.',
       reporters: [
         { type: 'html' },
